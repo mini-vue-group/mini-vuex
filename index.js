80ec05fd4c970ef466ba7977b293f552e4561343
cinyearchan <cinyearchan@gmail.com>
2021-09-28 18:55:05 +0800

    vuex install

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ class Store {
 
 function install (Vue) {
   _Vue = Vue
+  _Vue.mixin({
+    beforeCreate () {
+      if (this.$options.store) {
+        _Vue.prototype.$store = this.$options.store
+      }
+    }
+  })
 }
 
 export default { Store, install }
